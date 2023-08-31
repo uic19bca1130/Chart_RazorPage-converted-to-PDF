f742aae6c07847048ccd759deed04dcaa0688b51
Ankush <128672958+uic19bca1130@users.noreply.github.com>
2023-08-31 21:05:03 +0530

    Improve the quality of bar chart and remved the undefined above the data table and Add code for line chart using SVG and applied function to download the bar chart and line chart SVG with data tables in PDF.

---
+++ Chart_RazorP/Controller/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Chart_RazorP.Controllers
+{
+	public class HomeController : Controller
+	{
+		// ... other methods ...
+
+		[HttpGet("/getTableData")]
+		public IActionResult GetTableData()
+		{
+			var tableData = new List<object>
+			{
+				new { ID = 1, Name = "sidhu", Phone = "123-456-7890", Email = "sidhu@example.com" },
+				new { ID = 2, Name = "mooseala", Phone = "987-654-3210", Email = "mooseala@example.com" },
+                // Add More Rows If You Need
+            };
+
+			return Json(tableData);
+		}
+	}
+}
